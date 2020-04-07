7309108a9e64f5ed45ea89037dfac663896925d1
MistaC <csy385@mocs.utc.edu>
2020-04-06 23:35:07 -0400

    Encryption function working
    The encryption function now works. It should be able to handle any symbol found in string.printable (100 symbols in total). Still need to add key exporting (in a file) and decryption function.

--- encrypt.py
+++ encrypt.py
@@ -4,15 +4,15 @@ from sympy import Matrix
 import secrets
 
 #This module provides both encryption and decryption functionality for the Hill cipher.
-# TODO: Simplify code to streamline understanding of functions and reduce non-built-in dependencies.
 # TODO: Document usage of each function where necessary.
 # TODO: Move all code into functions.
+# TODO: Changeover from using the ascii values to the printable characters values.
 
 # gives int index for letter in alphabet
-def letterIndex(letter):
-    return string.ascii_lowercase.index(letter)
-def indexLetter(index):
-    return chr(int(index) +97 )
+def charIndex(char):
+    return string.printable.index(char)
+def indexChar(index):
+    return string.printable[index]
 
 # def createKey():
 #     keyWord = input("Input Key: ")
@@ -37,30 +37,76 @@ def indexLetter(index):
 #     return [matrixRows,matrixColumns]
 
 
-#Returns cryptographically secure square list of the cipher key. May not work on all systems. Argument from argv.
-def genKey(msg_height):
-    key_matrix = [msg_height][msg_height]
-    for row in key_matrix:
-        for column in key_matrix:
-            key_matrix[row][column] = 97+secrets.randbelow(25) #get value of an ascii lowercase alphabet symbol
+#Returns cryptographically secure 3x3 square list of the cipher key. May not work on all systems.
+def genKey():
+    key_matrix = np.zeros((3,3),dtype=np.int)
+    for row in range(3):
+        for col in range(3):
+            key_matrix[row][col] = 0+secrets.randbelow(100) #Addressing from 0-99 for printable characters
     return key_matrix
-def encrypt(key,msg):
-    pass
+
+
+#Encrypts message in blocks of 3.
+def encrypt(key,msg): #key is key matrix. msg is plaintext string.
+    temp = np.zeros((3,1), dtype=np.int) #holds msg character ascii values
+    encrypted_text = ''
+    iter = 0
+    print("The plaintext is: {}".format(msg))
+    for c in msg: #Loop three times. Get a character ascii value each time. Do matrix mult at 3. Convert resulting values into letters and concat with text variable.
+        if iter > 2: #3 ascii values already in temp
+            mult = np.matmul(key,temp) #3x3 * 3x1 = 3x1
+            mult[0][0] = mult[0][0] % 100
+            mult[1][0] = mult[1][0] % 100
+            mult[2][0] = mult[2][0] % 100
+            encrypted_text += indexChar(mult[0][0])
+            encrypted_text += indexChar(mult[1][0])
+            encrypted_text += indexChar(mult[2][0])
+            iter = 0
+        temp[iter][0] = charIndex(c)
+        iter += 1
+    if iter > 0: #Exited loop with partially or fully filled block of three values.
+        if iter == 1:
+            #single value in temp
+            mult = np.matmul(key,temp) #3x3 * 3x1 = 3x1
+            mult[0][0] = mult[0][0] % 100
+            encrypted_text += indexChar(mult[0][0])
+        elif iter == 2:
+            #two values in temp
+            mult = np.matmul(key,temp) #3x3 * 3x1 = 3x1
+            mult[0][0] = mult[0][0] % 100
+            mult[1][0] = mult[1][0] % 100
+            encrypted_text += indexChar(mult[0][0])
+            encrypted_text += indexChar(mult[1][0])
+        elif iter == 3:
+            mult = np.matmul(key,temp) #3x3 * 3x1 = 3x1
+            mult[0][0] = mult[0][0] % 100
+            mult[1][0] = mult[1][0] % 100
+            mult[2][0] = mult[2][0] % 100
+            encrypted_text += indexChar(mult[0][0])
+            encrypted_text += indexChar(mult[1][0])
+            encrypted_text += indexChar(mult[2][0])
+        else:
+            print("Something went wrong in the encryption function.")
+    return encrypted_text
+
+
+# TODO: FINISH DECRYPTION FUNCTION
+#Decrypts message in blocks of 3.
 def decrypt(key,msg):
     pass
 
+
+# TODO: Rewrite as two functions, one for encryption and decryption. Both take a text file object, but decryption also takes a key file object.
 def main():
-    # with open('test.txt') as f:
-    #     t = f.read()
-    # print(t.strip())
     with open('test.txt') as f:
         msg = f.read().strip()
-        msg_list = [][]
-        for k,v in msg:
-            if v != ' ':
-                msg_list[k][0] = v #expansion of rows, not columns
-    msg_height = len(msg_list) #gets number of rows
-    key = genKey(msg_height)
+    # print(genKey())
+    key = genKey()
+    ciphertext = encrypt(key,msg)
+    print(ciphertext)
+    # everything = string.printable
+    # for symbol in everything:
+    #     print("{}: {}".format(everything.index(symbol),symbol))
 main()
 
 
