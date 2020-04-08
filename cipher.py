b0e82617c02394a54803919f1f78c71b750741a2
MistaC <csy385@mocs.utc.edu>
2020-04-07 21:39:32 -0400

    Partially working decrypt function
    Decryption function now has limited functionality. It will correctly decrypt any ciphertext so long as that text can be split into blocks of 3 characters. Incomplete 3-blocks will not be correctly decrypted.

---
+++ cipher.py
@@ -0,0 +1,148 @@
+import numpy as np
+import string
+from sympy import Matrix
+import secrets
+
+#This module provides both encryption and decryption functionality for the Hill cipher.
+# TODO: Document usage of each function where necessary.
+# TODO: Move all code into functions.
+
+# gives int index for letter in alphabet
+def charIndex(char):
+    return string.printable.index(char)
+def indexChar(index):
+    return string.printable[index]
+
+
+#Returns cryptographically secure 3x3 square list of the cipher key. May not work on all systems.
+def genKey():
+    key_matrix = np.zeros((3,3),dtype=np.int)
+    while True:
+        try:
+            for row in range(3):
+                for col in range(3):
+                    key_matrix[row][col] = 0+secrets.randbelow(100) #Addressing from 0-99 for printable characters
+            inverse_key = Matrix(key_matrix).inv_mod(100)
+            inverse_key = np.array(inverse_key)
+            inverse_key = inverse_key.astype(int)
+            break
+        except ValueError:
+            print("Key is not invertible. Trying another...")
+    return key_matrix
+
+
+#Encrypts message in blocks of 3.
+def encrypt(key,msg): #key is key matrix. msg is plaintext string.
+    temp = np.zeros((3,1), dtype=np.int) #holds msg character ascii values
+    encrypted_text = ''
+    iter = 0
+    print("The plaintext is: {}".format(msg))
+    for c in msg: #Loop three times. Get a character ascii value each time. Do matrix mult at 3. Convert resulting values into letters and concat with text variable.
+        if iter > 2: #3 ascii values already in temp
+            mult = np.matmul(key,temp) #3x3 * 3x1 = 3x1
+            mult[0][0] = mult[0][0] % 100
+            mult[1][0] = mult[1][0] % 100
+            mult[2][0] = mult[2][0] % 100
+            encrypted_text += indexChar(mult[0][0])
+            encrypted_text += indexChar(mult[1][0])
+            encrypted_text += indexChar(mult[2][0])
+            iter = 0
+        temp[iter][0] = charIndex(c)
+        iter += 1
+    if iter > 0: #Exited loop with partially or fully filled block of three values.
+        if iter == 1:
+            #single value in temp
+            mult = np.matmul(key,temp) #3x3 * 3x1 = 3x1
+            mult[0][0] = mult[0][0] % 100
+            encrypted_text += indexChar(mult[0][0])
+        elif iter == 2:
+            #two values in temp
+            mult = np.matmul(key,temp) #3x3 * 3x1 = 3x1
+            mult[0][0] = mult[0][0] % 100
+            mult[1][0] = mult[1][0] % 100
+            encrypted_text += indexChar(mult[0][0])
+            encrypted_text += indexChar(mult[1][0])
+        elif iter == 3:
+            mult = np.matmul(key,temp) #3x3 * 3x1 = 3x1
+            mult[0][0] = mult[0][0] % 100
+            mult[1][0] = mult[1][0] % 100
+            mult[2][0] = mult[2][0] % 100
+            encrypted_text += indexChar(mult[0][0])
+            encrypted_text += indexChar(mult[1][0])
+            encrypted_text += indexChar(mult[2][0])
+        else:
+            print("Something went wrong in the encryption function.")
+    return encrypted_text
+
+
+# TODO: FINISH TESTING DECRYPTION FUNCTION
+#Decrypts message in blocks of 3.
+def decrypt(key,msg):
+    temp = np.zeros((3,1), dtype=np.int)
+    decrypted_text = ''
+    iter = 0
+    print("The encrypted text is: {}".format(msg))
+    for c in msg:
+        print("Current symbol: ",c)
+        if iter > 2:
+            mult = np.matmul(key,temp)
+            mult[0][0] = mult[0][0] % 100
+            mult[1][0] = mult[1][0] % 100
+            mult[2][0] = mult[2][0] % 100
+            # print("0,0: {}\n1,0: {}\n2,0: {}\nData types: {}, {}, {}".format(mult[0][0],mult[1][0],mult[2][0],type(mult[0][0]),type(mult[1][0]),type(mult[2][0])))
+            decrypted_text += indexChar(mult[0][0])
+            decrypted_text += indexChar(mult[1][0])
+            decrypted_text += indexChar(mult[2][0])
+            iter = 0
+        temp[iter][0] = charIndex(c)
+        # print("Index of {} is {}.".format(c,charIndex(c)))
+        iter += 1
+    if iter > 0: #Exited loop with partially or fully filled block of three values.
+        if iter == 1:
+            #single value in temp
+            # print("Before mult: {}".format(temp[0][0]))
+            mult = np.matmul(key,temp)
+            # print("After mult & before mod: {}".format(mult[0][0]))
+            mult[0][0] = mult[0][0] % 100
+            # print("After mult & mod: {}".format(mult[0][0]))
+            decrypted_text += indexChar(mult[0][0])
+        elif iter == 2:
+            #two values in temp
+            mult = np.matmul(key,temp)
+            mult[0][0] = mult[0][0] % 100
+            mult[1][0] = mult[1][0] % 100
+            decrypted_text += indexChar(mult[0][0])
+            decrypted_text += indexChar(mult[1][0])
+        elif iter == 3:
+            mult = np.matmul(key,temp)
+            mult[0][0] = mult[0][0] % 100
+            mult[1][0] = mult[1][0] % 100
+            mult[2][0] = mult[2][0] % 100
+            decrypted_text += indexChar(mult[0][0])
+            decrypted_text += indexChar(mult[1][0])
+            decrypted_text += indexChar(mult[2][0])
+        else:
+            print("Something went wrong in the decryption function.")
+    return decrypted_text
+
+
+def main(): #for testing only. Will be removed in the final cipher file, as the GUI will do all the function calling.
+    with open('test.txt') as f:
+        msg = f.read().strip()
+
+    key = genKey()
+    # print("Original key matrix:\n",key)
+    # print("Bad inverse with numpy:\n",np.linalg.inv(key))
+    inverse_key = Matrix(key).inv_mod(100)
+    inverse_key = np.array(inverse_key)
+    inverse_key = inverse_key.astype(int)
+    # print("The better inverse:\n",inverse_key)
+
+    ciphertext = encrypt(key,msg)
+    print("Ciphertext: ",ciphertext)
+
+    decrypted_text = decrypt(inverse_key,ciphertext)
+    print("The unencrypted text is: {}".format(decrypted_text))
+    # for c in string.printable:
+    #     print("{}: {}".format(string.printable.index(c),c))
+main()
