7e6edac79d6ec8c51b31cecc0dab451d76958e63
MistaC <csy385@mocs.utc.edu>
2020-04-11 14:33:08 -0400

    Changes to file handling in GUI.
    Correctly stores and accesses decryption key now. Some key-based performance issues for decryption were noticed, however, as the encrypt.decrypt algorithm may not be perfect. Currently testing this.

--- gui.py
+++ gui.py
@@ -1,7 +1,9 @@
 from tkinter import *
 from encrypt import *
+import numpy as np
 
 mainWindow = Tk()
+mainWindow.geometry("500x200")
 msg = Entry(mainWindow)
 msg.pack()
 def encryptClick():
@@ -11,21 +13,37 @@ def encryptClick():
 		inverseKey = Matrix(key).inv_mod(100)
 		inverseKey = np.array(inverseKey)
 		inverseKey = inverseKey.astype(int)
-		inverseKey.tofile(keyGenFile)
+		# inverseKey.tofile(keyGenFile) #Issue occurs here
+		for i in range(3):
+		    for j in range(3):
+		        keyGenFile.write("{}:".format(inverseKey[i][j]))
 		keyGenFile.close()
 		cipherText = encrypt(key,msg.get())
 		ciphertextWriteFile.write(cipherText)
 		ciphertextWriteFile.close()
-		complete = Label(mainWindow, text="Encryption complete")
-		complete.pack()
-		print(cipherText)
+		print("---------------\nEncryption complete! Ciphertext below.\n\n{}\n---------------".format(cipherText))
+		# complete = Label(mainWindow, text="Encryption complete")
+		# complete.pack()
+		# print(cipherText)
 def decryptClick():
 		readKey = open("key.txt", "r")
 		readMessage = open("codedmessage.txt", "r")
 
-		decryptedText = decrypt(readKey.read(),readMessage.read())
-		complete = Label(mainWindow, text=decryptedText)
-		complete.pack()
+		inv_key = readKey.read().split(":")
+		inv_key = [x for x in inv_key if x != ":"]
+		inv_key = [x for x in inv_key if x != ""]
+		dec_key = [[0 for j in range(3)] for i in range(3)]
+		iter = 0
+		for i in range(3):
+		    for j in range(3):
+		        dec_key[i][j] = int(inv_key[iter])
+		        iter += 1
+		final_key = np.asarray(dec_key)
+		# print(final_key)
+		decryptedText = decrypt(final_key,readMessage.read())
+		print("---------------\nDecryption complete! Plaintext below.\n\n{}\n---------------".format(decryptedText))
+		# complete = Label(mainWindow, text=decryptedText)
+		# complete.pack()
 		readKey.close()
 		readMessage.close()
 encryptButton = Button(mainWindow,text="encrypt", command=encryptClick)
@@ -33,11 +51,3 @@ decryptButton = Button(mainWindow,text="decrypt", command=decryptClick)
 encryptButton.pack()
 decryptButton.pack()
 mainWindow.mainloop()
-
-
-
-
-
-
-
-
