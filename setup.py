3c230f3211e9d6b80c7b1c3ed4b36fe3a588558f
MistaC <csy385@mocs.utc.edu>
2020-04-12 16:35:59 -0400

    Executible conversion of program.
    All files are located under the "build" directory. Launch program through "gui.exe". Message text file included. If you want to use the program outside of the github repo, you must move the entire "build" directory, as it contains files used by the gui.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from cx_Freeze import setup, Executable
+
+base = None
+
+executables = [Executable("gui.py", base=base)]
+
+packages = ["idna","tkinter","encrypt","numpy"]
+options = {
+    'build_exe': {
+        'packages':packages,
+    },
+}
+
+setup(
+    name = "Hill Cipher GUI",
+    options = options,
+    version = "3.8",
+    description = 'Launches the Hill Cipher GUI',
+    executables = executables
+)
